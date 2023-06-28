9be47daac5332e6e9f61c923561b2821c7ef1eaf
이은성 <110223414+SeongLI@users.noreply.github.com>
2023-06-28 21:32:57 +0900

    [Silver I] Title: 경로 찾기, Time: 452 ms, Memory: 30028 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+	public static int N; // 정점의 개수
+	public static int[][] map; // 인접행렬
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		map = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		} // input 끝
+
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][k] == 1 && map[k][j] == 1) {
+						map[i][j] = 1;
+					}
+				}
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(map[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+
+	} // main 끝
+}
