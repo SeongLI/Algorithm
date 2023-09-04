1ed8a97706fc676e2b52253a3f69d11a6b1060d6
이은성 <110223414+SeongLI@users.noreply.github.com>
2023-09-04 19:56:18 +0900

    [unrated] Title: l로 만들기, Time: 9.03 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        char[] ch_arr = myString.toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < ch_arr.length ; i++){
+            if(ch_arr[i] < 'l'){
+                ch_arr[i] = 'l';
+            }
+            sb.append(ch_arr[i]);
+        }
+
+        return sb.toString();
+    }
+}
