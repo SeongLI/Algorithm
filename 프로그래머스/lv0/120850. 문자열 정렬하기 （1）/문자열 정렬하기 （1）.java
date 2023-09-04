7e128a35c9887c100dbf23e8536ec845d27ad527
이은성 <110223414+SeongLI@users.noreply.github.com>
2023-09-04 23:31:54 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.61 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -1,22 +1,30 @@
 import java.util.*;
 class Solution {
     public int[] solution(String my_string) {
-        Queue<Integer> q = new LinkedList<>();
-        
-        String[] str_arr = my_string.split("");
-        for(int i = 0 ; i < str_arr.length ; i++){
-            if(Character.isDigit(str_arr[i].charAt(0))){
-                int temp = Integer.parseInt(str_arr[i]);
-                q.add(temp);
-                }
-            }
-        
-        int len = q.size();
-        int[] answer = new int[len];
-        for(int i = 0 ; i < len ; i++){
-            answer[i] = q.poll();
+        my_string = my_string.replaceAll("[a-z]", "");    
+        int[] answer = new int[my_string.length()];
+        for(int i = 0 ; i < my_string.length() ; i++){
+            answer[i] = my_string.charAt(i)-48;
         }
         Arrays.sort(answer);
         return answer;
+        
+//         Queue<Integer> q = new LinkedList<>();
+        
+//         String[] str_arr = my_string.split("");
+//         for(int i = 0 ; i < str_arr.length ; i++){
+//             if(Character.isDigit(str_arr[i].charAt(0))){
+//                 int temp = Integer.parseInt(str_arr[i]);
+//                 q.add(temp);
+//                 }
+//             }
+        
+//         int len = q.size();
+//         int[] answer = new int[len];
+//         for(int i = 0 ; i < len ; i++){
+//             answer[i] = q.poll();
+//         }
+//         Arrays.sort(answer);
+//         return answer;
     }
 }
