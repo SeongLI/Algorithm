0e52cbe8f04f8dfa287523ad309bd1a0cc40c958
이은성 <110223414+SeongLI@users.noreply.github.com>
2023-09-05 09:38:59 +0900

    [unrated] Title: 접미사 배열, Time: 0.17 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_string) {
+        int len = my_string.length();
+        String[] answer = new String[len];
+        for(int i = len-1 ; i > -1 ; i--){
+            answer[i] = my_string.substring(i, len);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
