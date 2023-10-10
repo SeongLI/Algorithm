dfea232992cbb722ede23e1d1c6d5a125b4f4e07
이은성 <110223414+SeongLI@users.noreply.github.com>
2023-10-10 21:27:27 +0900

    [level unrated] Title: 모스부호 (1), Time: 10.27 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/unrated/120838. 모스부호 （1）/모스부호 （1）.java
@@ -9,13 +9,16 @@ class Solution {
     "...","-","..-","...-",".--","-..-",
     "-.--","--.." };
         
-        StringBuilder sb = new StringBuilder();
+        // StringBuilder sb = new StringBuilder();
+        String result = "";
         for(int i = 0 ; i < str_arr.length ; i++){
             String temp = str_arr[i];
             for(int j = 0 ; j < morse_arr.length ; j++){
-                if(temp.equals(morse_arr[j])) sb.append(Character.toString(j+'a'));
+                // if(temp.equals(morse_arr[j])) sb.append(Character.toString(j+'a'));
+                if(temp.equals(morse_arr[j])) result += (char)(j+'a');
             }
         }
-        return sb.toString();
+        // return sb.toString();
+        return result;
     }
 }
