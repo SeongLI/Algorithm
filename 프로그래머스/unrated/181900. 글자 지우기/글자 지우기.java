c3c57795462b61c4ba2f5074483a1fc754be1a08
이은성 <110223414+SeongLI@users.noreply.github.com>
2023-10-13 00:12:49 +0900

    [level unrated] Title: 글자 지우기, Time: 2.01 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -1,15 +1,30 @@
+import java.util.Arrays;
 class Solution {
     public String solution(String my_string, int[] indices) {
-        String[] ch_arr = my_string.split("");
-        String temp = "X";
+        // String[] ch_arr = my_string.split("");
+        // String temp = "X";
+        // for(int i = 0 ; i < indices.length ; i++){
+        //     int index = indices[i];
+        //     ch_arr[index] = temp;
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // for(int i = 0 ; i < ch_arr.length ; i++){
+        //     if(!ch_arr[i].equals(temp)) sb.append(ch_arr[i]);
+        // }
+        // return sb.toString();
+        String result = "";
+        String[] Str_arr = my_string.split("");
+        System.out.println(indices.length);
+        System.out.println(Arrays.toString(Str_arr));
         for(int i = 0 ; i < indices.length ; i++){
-            int index = indices[i];
-            ch_arr[index] = temp;
+            int Index = indices[i];
+            Str_arr[Index] = "";
         }
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < ch_arr.length ; i++){
-            if(!ch_arr[i].equals(temp)) sb.append(ch_arr[i]);
+        System.out.println(Str_arr.length);
+        System.out.println(Arrays.toString(Str_arr));
+        for(int i = 0 ; i < Str_arr.length; i++){
+            result += Str_arr[i];
         }
-        return sb.toString();
+        return result;
     }
 }
