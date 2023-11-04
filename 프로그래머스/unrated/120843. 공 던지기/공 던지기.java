b72dbdbaa7b6e6272ca119ade5725ddad1825e24
이은성 <110223414+SeongLI@users.noreply.github.com>
2023-11-04 14:29:53 +0900

    [level unrated] Title: 공 던지기, Time: 0.04 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        // 1번 (index 0번) 부터 시작
+        // 2칸을 건너서 반복
+        // k번째 던지는 사람의 numbers[index];
+       
+        if(k==1) return 1;
+        
+        int tempIndex = 0;
+        for(int i = 0 ; i < k-1 ; i++){
+            tempIndex += 2;
+            tempIndex %= numbers.length;
+            
+        }
+        return tempIndex + 1;
+    
+    }
+}
