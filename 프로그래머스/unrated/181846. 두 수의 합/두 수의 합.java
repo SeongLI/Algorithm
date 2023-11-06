d78a7d75a89aaf343304743927f227d5cde3bd19
이은성 <110223414+SeongLI@users.noreply.github.com>
2023-11-07 00:26:37 +0900

    [level unrated] Title: 두 수의 합, Time: 236.43 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,14 @@
+import java.math.*;
+class Solution {
+    public String solution(String a, String b) {
+        
+        BigInteger A = new BigInteger(a);
+        BigInteger B = new BigInteger(b);
+        
+        BigInteger answer = A.add(B);
+        
+        return answer.toString();
+
+        
+    }
+}
