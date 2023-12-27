8fdae907f396000123386f71a97a8fd996a6c734
이은성 <110223414+SeongLI@users.noreply.github.com>
2023-12-27 14:43:39 +0900

    [level 1] Title: 폰켓몬, Time: 4.30 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int result_len = nums.length / 2 ;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0 ; i < nums.length ; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 1));
+        }
+        
+        int count = 0;
+        for(Integer index : map.keySet()){
+            if(map.get(index) == 1) count ++;
+        }
+        
+        if(count <= result_len) return count;
+        else return result_len;
+    }
+}
