744e0d1060110968117a8ebd3a52c15c712fa1ff
이은성 <110223414+SeongLI@users.noreply.github.com>
2024-01-08 23:08:15 +0900

    [level unrated] Title: 배열 만들기 6, Time: 18.65 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -3,36 +3,25 @@ class Solution {
     public int[] solution(int[] arr) {
         Stack<Integer> stack = new Stack<>();
         
-        int arr_len = arr.length;
-        int point = 0;
-        
-        for(int i = 0 ; i < arr_len; i++){
-            if(stack.isEmpty()) stack.add(arr[i]);
-            else{
-                if(stack.peek() == arr[i]){
+        for(int i = 0 ; i < arr.length ; i++){
+            int temp = arr[i];
+            if(stack.isEmpty()){
+                stack.push(temp);
+            }else{
+                int stack_temp = stack.peek();
+                if(stack_temp == temp){
                     stack.pop();
-                }
-                else{
-                    stack.push(arr[i]);
+                }else{
+                    stack.push(temp);
                 }
             }
-            // System.out.println(Arrays.toString(stack.toArray()));
         }
-        
         int size = stack.size();
-        // System.out.println(size);
-        if(size == 0) {
-            stack.push(-1);
-            size = 1;
-        }
-        int[] answer = new int[size];
+        if(size == 0) return new int[]{-1};
+        int[] ans = new int[size];
         for(int i = size-1 ; i >= 0 ; i--){
-            answer[i] = stack.pop();
+            ans[i] = stack.pop();
         }
-        
-        
-        return answer;
-        
-        
+        return ans;
     }
 }
