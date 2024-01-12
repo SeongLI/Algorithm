9b5d1a2fabc0c1bc63c25937b452e839db3f0c94
이은성 <110223414+SeongLI@users.noreply.github.com>
2024-01-12 14:53:04 +0900

    [level Lv.2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/Lv.2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,22 +1,30 @@
+// 앞문자의 마지막과 뒷문자의 처음을 비교한다. charAt
+// 이전에 나왔던 것인지 확인한다. hashmap
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put(words[0], 0);
         int[] result = new int[2];
-        int len = words.length;
-        LinkedList<String> list = new LinkedList<>();
-        list.add(words[0]);
+        int index = 1; // 인덱스
         
-        for(int i = 1 ; i < len ; i++){
-            char before = words[i-1].charAt(words[i-1].length()-1);
-            char start = words[i].charAt(0);
-        
-            if(before != start || list.contains(words[i])){
-                result[0] = i % n + 1 ;
-                result[1] = i / n + 1;
-                return result;
+        while(index < words.length){
+            // 앞
+            char front = words[index-1].charAt(words[index-1].length()-1);
+            // 뒤(현재 기준)
+            char back = words[index].charAt(0);
+            // 단어 체크
+            int check = map.getOrDefault(words[index], -1);
+
+            if((check != -1) || (front != back)){
+                result[0] = index % n + 1; // 번호
+                result[1] = index / n + 1; // 차례
+                break;
             }
-            list.add(words[i]);
+            map.put(words[index], 1);
+            index++;
         }
+        
         return result;
     }
 }
