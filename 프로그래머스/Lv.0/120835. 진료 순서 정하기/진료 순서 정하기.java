e1452a9651661946ec01b63549634acd84ad2c62
이은성 <110223414+SeongLI@users.noreply.github.com>
2024-01-12 11:53:12 +0900

    [level Lv.0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/Lv.0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,25 +1,16 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] emergency) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] new_emer = new int[emergency.length];
-    
-        for(int i = 0 ; i < new_emer.length ; i++){
-            new_emer[i] = emergency[new_emer.length - 1 -i];
-        }
+        int len = emergency.length;
+        int[] count = new int[len];
         
-        Arrays.sort(new_emer);
-        for(int i = 0 ; i < emergency.length ; i++){
-            map.put(new_emer[i], emergency.length - i);
+        for(int i = 0 ; i< len ; i++){
+            for(int j = 0 ; j < len ; j++){
+                if(emergency[i] <= emergency[j] ){
+                    count[i]++;
+                }
+            }
         }
-        System.out.println(Arrays.toString(emergency));
-        System.out.println(Arrays.toString(new_emer));
+        return count;
         
-        int[] result = new int[new_emer.length];
-        for(int i = 0 ; i < result.length ; i++){
-            result[i] = map.get(emergency[i]);
-        }
-        
-        return result;
     }
 }
