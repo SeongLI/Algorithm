6334526fff1f1b43c5ab17f9275dbf8ff036b1ed
이은성 <110223414+SeongLI@users.noreply.github.com>
2024-01-31 10:17:53 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,13 @@
+-- 공간을 둘 이상 등록한 사람
+-- 아이디 순으로 조회 ASC
+
+SELECT *
+FROM PLACES
+WHERE HOST_ID IN (
+    SELECT HOST_ID
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(HOST_ID) >= 2
+    )
+ORDER BY ID ASC
+;
