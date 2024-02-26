a1abbe11c5de80c513f7fa1379152b8f91df732a
이은성 <110223414+SeongLI@users.noreply.github.com>
2024-02-26 14:44:07 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,13 +1,18 @@
--- 희귀도가 'RARE'인 아이템들의 모든 다음 업그레이드 아이템
--- 아이템 ID(ITEM_ID), 아이템 명(ITEM_NAME), 아이템의 희귀도(RARITY)를 출력
+-- 아이템의 희귀도가 'RARE'인 아이템의 모든 다음 업그레이드 아이템
+-- 아이템 ID(ITEM_ID), 아이템 명(ITEM_NAME), 아이템의 희귀도(RARITY) 출력
+-- 아이템 ID 기준 DSEC
 
 SELECT ITEM_ID, ITEM_NAME, RARITY
-FROM ITEM_INFO 
+FROM ITEM_INFO
 WHERE ITEM_ID IN (
-    SELECT T.ITEM_ID
-    FROM ITEM_INFO AS I JOIN ITEM_TREE AS T
-    ON I.ITEM_ID = T.PARENT_ITEM_ID
-    WHERE I.RARITY = 'RARE'
+    SELECT ITEM_ID
+    FROM ITEM_TREE 
+    WHERE PARENT_ITEM_ID IN (
+        SELECT ITEM_ID
+        FROM ITEM_INFO
+        WHERE RARITY = 'RARE'
+    )
 )
 ORDER BY ITEM_ID DESC
 ;
+    
