386d31952ea7d57d84d32537917606e315f60318
이은성 <110223414+SeongLI@users.noreply.github.com>
2024-02-29 17:33:54 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,12 +1,20 @@
--- 음식종류 별로
--- 즐겨찾기 수가 가장 많은 식당의
--- 음식 종류, ID, 식당 이름, 즐겨찾기수를 조회
--- 음식 종류를 기준으로 내림차순 정렬
+-- 음식종류별로 즐겨찾기수가 가장 많은 식당
+-- 음식 종류, ID, 식당 이름, 즐겨찾기수 조회
+-- 음식 종류 기준 DESC
 
-SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES FROM REST_INFO
+# SELECT FOOD_TYPE, REST_ID, REST_NAME, MAX(FAVORITES) AS FAVORITES
+# FROM REST_INFO
+# GROUP BY FOOD_TYPE 
+# ORDER BY FOOD_TYPE DESC
+# ;
+# 틀린 이유 => FOOD_TYPE으로 그룹화할 경우, 각 FOOD_TYPE 그룹 내에 여러 개의 REST_ID와 REST_NAME이 존재할 수 있다.
+
+SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+FROM REST_INFO
 WHERE (FOOD_TYPE, FAVORITES) IN (
-    SELECT FOOD_TYPE, MAX(FAVORITES) AS FAVORITES FROM REST_INFO
+    SELECT FOOD_TYPE, MAX(FAVORITES) AS FAVORITES
+    FROM REST_INFO
     GROUP BY FOOD_TYPE
-    )    
+)
 ORDER BY FOOD_TYPE DESC
-    
+;
