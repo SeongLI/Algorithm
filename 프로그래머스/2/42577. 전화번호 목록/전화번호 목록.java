41d2a3aa7f7a5738ae1e74b398675c636b7397a4
이은성 <110223414+SeongLI@users.noreply.github.com>
2024-03-15 16:57:07 +0900

    [level 2] Title: 전화번호 목록, Time: 295.63 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,16 +1,42 @@
-import java.util.*;
+// 해시맵  풀이
+// import java.util.*;
+// class Solution {
+//     public boolean solution(String[] phone_book) {
+//         Arrays.sort(phone_book);
+//         HashMap<String, Integer> map = new HashMap<>();
+        
+//         for(int i = 0 ; i < phone_book.length ; i++){
+//             map.put(phone_book[i], i);
+//         }
+        
+//         for(int i = 0 ; i < phone_book.length ; i++){
+//             String temp = phone_book[i];
+//             for(int j = 0 ; j < temp.length() ; j++){
+//                 if(map.containsKey(temp.substring(0, j))) return false; // 0부터 j 전까지
+//             }
+//         }
+        
+//         return true;
+//     }
+// }
 
+// 배열 풀이
+import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
         
-        boolean answer = true;
+        // 시간 초과!
+        // for(int i = 0 ; i < phone_book.length ; i++){
+        //     String temp = phone_book[i];
+        //     for(int j = i+1 ; j < phone_book.length ; j++){
+        //         if(phone_book[j].startsWith(temp)) return false;
+        //     }
+        // }
+        
         for(int i = 0 ; i < phone_book.length-1 ; i++){
-                if(phone_book[i+1].startsWith(phone_book[i])) {
-                    answer = false;
-                    break;
-                 }
+            if(phone_book[i+1].startsWith(phone_book[i])) return false;
         }
-        return answer;
+        return true;
     }
 }
